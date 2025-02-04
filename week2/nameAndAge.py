e8cfa47a6481db2a101fd81d26547fe794c1cad1
irene celebrin <irene.celebrin@gmail.com>
2025-02-04 20:42:19 +0000

    NameAndAge + some maths

---
+++ week2/nameAndAge.py
@@ -0,0 +1,25 @@
+# nameAndAge.py
+# lab 2.2
+# print input name and age 
+# created by: irene celebrin 
+
+#Write a program (nameAndAge.py) that reads in a name and age and outputs the following (make sure you get the spaces right):
+# add a tab after the age 
+
+'''
+name = input('Please enter your name: ')
+age = input('Please enter your age: ')
+
+print(f'Hello {name}, your age is \t{age}.')
+'''
+
+#Write a program that outputs 21 -4
+'''
+number = 21 - 4
+
+print(number)
+'''
+
+# . Write a program that outputs if 2 == 3
+check = 2 > 3 
+print(check)
