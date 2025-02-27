66b5c24ae13ff8d68c7c6869e7cb6530d23f6791
irene celebrin <irene.celebrin@gmail.com>
2025-02-27 19:40:33 +0000

    fix lab week 4

--- Labs/week04-flow/lab4.2.6_topthree.py
+++ Labs/week04-flow/lab4.2.6_topthree.py
@@ -10,6 +10,7 @@ import random
 # create list with empty random numbers 
 random_numbers = []
 
+'''
 # generate a random number until the length of the list is 10 
 while len(random_numbers) < 10:
      random_number = random.randint(0,100)
@@ -19,12 +20,19 @@ while len(random_numbers) < 10:
 for random_number in random_numbers: 
      print(random_number)
      random_number += 1
+'''
+#create for loop to create a random number between 0 and 100 for the range 1 to 10, and print list
+for i in range(0,10):
+    random_numbers.append(random.randint(0,100))
+print(f'10 random numbers\t {random_numbers}')
 
 #sort list in descending order
 sorted_random_numbers = sorted(random_numbers, reverse=True)
 
 # print only the first 3 elements of the sorted list 
+top_three = []
 count = 0 
 while count < 3: 
-    print(sorted_random_numbers[count])
+    top_three.append(sorted_random_numbers[count])
     count += 1
+print(f'The top 3 are\t\t {top_three}')
