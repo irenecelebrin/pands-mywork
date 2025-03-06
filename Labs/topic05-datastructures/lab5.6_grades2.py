7685808fb228fd55fc189dce6e079188ae4e669c
irene celebrin <irene.celebrin@gmail.com>
2025-03-06 19:18:01 +0000

    lab week 5, exercise 6

--- Labs/topic05-datastructures/lab5.6_grades2.py
+++ Labs/topic05-datastructures/lab5.6_grades2.py
@@ -6,4 +6,43 @@ student_name is blank.
 Next week we will be looking at functions and we will implement something
 like this.'''
 
+# prepare dictionnary 
+student = {
+    'name' : '',
+    'modules' : []
+}
+
+#ask user to enter their name
+name = input('Hello student! Please enter your name: ')
+
+def grades():
+
+    #add name to the dictionary 
+    student['name'] = name 
+
+    #ask the student to enter the module name and repreat the question until the answer is null
+    module = input('Enter module name: ')
+    while  module != '':
+        #append the module name in the list 
+        student['modules'].append({'course_name' : module})
+        #ask to enter another course name 
+        module = input('Enter module name: ')
+    
+    #ask student to enter the grades for each course she  entered 
+    for module in student['modules']:
+        grade = input('Please enter your grade for {}: '.format(module['course_name']))
+        module['grade'] = grade
+       
+    #print: student name 
+    print(f"Student\t: {student['name']}")
+        #print: course name: grade
+    for module in student['modules']: 
+        print("{}\t: {}".format(module['course_name'],module['grade']))
+
+
+while name != '':
+    grades()
+    name = input('Hello student! Please enter your name: ')
+
+
 
